44e39cd816e2a7e80fad5f8d6401eb4ffc42366a
Patrick de Campos Nascimento <patrickcampos77@hotmail.com>
2022-01-29 13:20:46 -0300

    Ajustando nomenclatura dos resultados imc

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,11 +28,9 @@ form.addEventListener('submit', function (e) {
 });
 
 function getNivelImc (imc) {
-  const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso',
-    'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+  const nivel = ['Magreza', 'Normal', 'Sobrepeso',
+    'Obesidade'];
 
-  if (imc >= 39.9) return nivel[5];
-  if (imc >= 34.9) return nivel[4];
   if (imc >= 29.9) return nivel[3];
   if (imc >= 24.9) return nivel[2];
   if (imc >= 18.5) return nivel[1];
